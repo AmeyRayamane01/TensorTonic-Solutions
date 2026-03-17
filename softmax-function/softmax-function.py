7f0127182366a1e1b01ec5b7cd9ed5d1b9939388
AmeyRayamane01 <147915470+AmeyRayamane01@users.noreply.github.com>
2026-03-17 11:35:26 +0530

    Add solution for softmax-function

---
+++ softmax-function/softmax-function.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def softmax(x):
+    x = np.asarray(x, dtype=float)
+
+    if x.ndim == 1:
+        x_shifted = x - np.max(x)
+        exp_x = np.exp(x_shifted)
+        return exp_x / np.sum(exp_x)
+
+    elif x.ndim == 2:
+        x_shifted = x - np.max(x, axis=1, keepdims=True)
+        exp_x = np.exp(x_shifted)
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
